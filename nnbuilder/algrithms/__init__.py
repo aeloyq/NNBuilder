1faf709bfe51e845dd294d61d26f12dadaac6965
aeloyq <aeloyq@outlook.com>
2017-10-22 11:04:56 +0800

    Important:New Kernel Supported

--- nnbuilder/algrithms/__init__.py
+++
@@ -1,8 +0,0 @@
-import adadelta
-import adagrad
-import adam
-import momentum
-import newtonsmethod
-import rmsprop
-import sgd
-__all__=['adadelta','adagrad','adam','momentum','newtonsmethod','rmsprop','sgd']
